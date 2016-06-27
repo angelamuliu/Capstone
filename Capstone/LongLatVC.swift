005185b703eb1065ca3c53a08b2806390ea245ff
Katherine Habeck <khabeck@smu.edu>
2016-06-27 13:37:44 -0400

    Added permissions code for longitude and latitude. Not connected to any VC

---
+++ Capstone/LongLatVC.swift
@@ -0,0 +1,61 @@
+//
+//  LongLatVC.swift
+//  Capstone
+//
+//  Created by Katherine Habeck on 6/27/16.
+//  Copyright © 2016 amliu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Foundation
+import CoreLocation
+
+class LongLatVC: UIViewController, CLLocationManagerDelegate {
+    
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        startLocation = nil
+        
+    }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        var latestLocation: AnyObject = locations[locations.count - 1]
+        
+        print(String(format: "%.4f",
+            latestLocation.coordinate.latitude))
+        print(String(format: "%.4f",
+            latestLocation.coordinate.longitude))
+        print(String(format: "%.4f", latestLocation.horizontalAccuracy))
+        print(String(format: "%.4f", latestLocation.altitude))
+        
+        
+        if startLocation == nil {
+            startLocation = latestLocation as! CLLocation
+        }
+        
+        //        var distanceBetween: CLLocationDistance =
+        //            latestLocation.distanceFromLocation(startLocation)
+        //
+        //        distance.text = String(format: "%.2f", distanceBetween)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func locationManager(manager: CLLocationManager!,
+                         didFailWithError error: NSError!) {
+        
+    }
+    
+}
+
--- Capstone/NotificationSettingsVC.swift
+++ Capstone/NotificationSettingsVC.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 amliu. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class NotificationSettingsVC: UIViewController {
 
