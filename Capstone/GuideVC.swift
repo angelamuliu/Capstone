392996d6e23bea22395604bab8c97edc2657d67e
Angela Liu <angelamuliu@gmail.com>
2016-07-19 16:22:15 -0400

    Guide first page is done, it loads from a guide and styled with autolayout and also has place minicards stacked on bottom with scroll

--- Capstone/GuideVC.swift
+++ Capstone/GuideVC.swift
@@ -3,6 +3,16 @@ import UIKit
 
 class GuideVC: UIViewController {
     
+    // UI Elements
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var stepLabel: UILabel! // # of pages / steps
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var contentStackView: UIStackView!
+
+    
     var guide:Guide?
     
     
@@ -12,7 +22,26 @@ class GuideVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO: Make category icon load from place
+        // TODO: Add in description to guide DB SCHEMA
+        
+        if guide != nil {
+            imageView.image = guide?.cardImage
+            nameLabel.text = guide?.title
+            categoryLabel.text = guide?.category
+            stepLabel.text = guide?.pages != nil ? "\(guide!.pages.count) steps" : "0 steps"
+            descriptionLabel.text = "Lorem Ipsum"
+            
+            // Present its places if it has any
+            for place in (guide?.places)! {
+                if let miniCard = NSBundle.mainBundle().loadNibNamed("MiniCardView", owner: self, options: nil).first as? MiniCardView {
+                    miniCard.useData(place)
+                    contentStackView.addArrangedSubview(miniCard)
+                }
+            }
+
+        }
     }
     
     override func didReceiveMemoryWarning() {
